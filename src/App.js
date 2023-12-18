0d5b3694c10577ba2119c72fbd04900c567747af
blamex321 <laxman2207@outlook.com>
2023-12-19 00:13:01 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Helmet } from "react-helmet";
+import Navbar from "./components/Navbar";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen">
+      <Helmet>
+        <title>Laxman's Portfolio</title>
+      </Helmet>
+      <div className="gradient-bg-welcome">
+        <Navbar />
+      </div>
     </div>
   );
 }
