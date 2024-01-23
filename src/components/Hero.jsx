4430a330b382a1d22003516ed655290cc118d770
blamex321 <laxman2207@outlook.com>
2024-01-23 09:24:06 +0530

    changes 1

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,10 +4,7 @@ import { Typewriter } from "react-simple-typewriter";
 
 const Hero = () => {
   const downloadResume = () => {
-    saveAs(
-      process.env.REACT_APP_RESUME_LINK,
-      "Laxman Bankupalle Resume.pdf"
-    );
+    saveAs(process.env.REACT_APP_RESUME_LINK, "Laxman Bankupalle Resume.pdf");
   };
 
   return (
@@ -16,23 +13,28 @@ const Hero = () => {
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <div className="h-auto">
             <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1 mb-4">
-              Hello, I'm a <br />{" "}
+              Hello, I'm <br />{" "}
               <Typewriter
                 words={[
                   "Laxman Bankupalle",
-                  "Cybersecurity Enthusiast",
-                  "Aspiring Code Wizard",
+                  "a Cybersecurity Enthusiast",
+                  "a Aspiring Code Wizard",
                 ]}
                 loop={0}
                 delaySpeed={2000}
               />
             </h1>
           </div>
-          <p className="mb-8 leading-relaxed">
-            Copper mug try-hard pitchfork pour-over freegan heirloom neutra air
-            plant cold-pressed tacos poke beard tote bag. Heirloom echo park
-            mlkshk tote bag selvage hot chicken authentic tumeric truffaut
-            hexagon try-hard chambray.
+          <p class="text-lg font-semibold mb-10">
+            Welcome to my portfolio! I'm Laxman, a dedicated Software Developer
+            and Cybersecurity enthusiast, passionate about pushing the
+            boundaries of innovation in web development. With a sharp eye for
+            detail and expertise in the MERN Stack, I've cultivated a diverse
+            skill set. My journey of continuous learning reflects my commitment
+            to crafting impactful solutions, not only in web development but
+            also in the dynamic world of cybersecurity. Join me on this visual
+            journey through my projects, each telling a unique story of skill,
+            passion, and dedication.
           </p>
           <div className="flex justify-center">
             <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
